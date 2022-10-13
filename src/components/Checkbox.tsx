b3b3f3ca7e4a6802a192071cb534d0bd2d2d3f0b
Luis Eduardo <lfdasilva9@gmail.com>
2022-10-13 17:02:03 -0300

    finish day 3 rockeseat

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,12 +1,12 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import {Check} from 'phosphor-react';
 
-export interface CheckBoxProps{}
+export interface CheckBoxProps extends Checkbox.CheckboxProps{}
 
-export function CheckBox({}: CheckBoxProps){
+export function CheckBox(props: CheckBoxProps){
     return(
         <Checkbox.Root 
-          className=" w-6 h-6 p-[2px] bg-gray-800 rounded">
+          className=" w-6 h-6 p-[2px] bg-gray-800 rounded"{...props}>
             <Checkbox.Indicator asChild>
                 <Check weight='bold' className='h-5 w-5 text-cyan-500'/>
             </Checkbox.Indicator>
