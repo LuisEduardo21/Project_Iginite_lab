c6386e43ac6cfd170f40b04db47ead9cb94ca67a
Luis Eduardo <lfdasilva9@gmail.com>
2022-10-13 00:25:10 -0300

    finish aula 02

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,32 @@
+import { TextInput, TextInputRootProps } from "./TextInput";
+import { Meta, StoryObj } from "@storybook/react";
+import { Envelope } from 'phosphor-react';
+
+export default {
+    title: 'components/TextInput',
+    component: TextInput.Root,
+    args: {
+        children: [
+            <TextInput.Icon>
+                < Envelope />
+            </TextInput.Icon >,
+            <TextInput.Input placeholder="Type your andress email" />
+        ]
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        }
+    }
+} as Meta<TextInputRootProps>;
+
+export const Default: StoryObj<TextInputRootProps> = {}
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+    args: {
+        children:  <TextInput.Input placeholder="Type your andress email" />
+    },
+}
+
